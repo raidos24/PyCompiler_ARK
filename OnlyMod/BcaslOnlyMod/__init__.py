2acccfe22219a606dd0c4e998e1168a009f7c8c3
Ague Samuel Amen <ague.samuel27@gmail.com>
2026-02-01 18:01:19 +0000

    miseen place de la structure pour bcaslonlymod

---
+++ OnlyMod/BcaslOnlyMod/__init__.py
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2026 Ague Samuel Amen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+BcaslOnlyMod - Module autonome pour gérer les plugins BCASL
+
+Interface complète pour exécuter et configurer les plugins BCASL
+indépendamment de l'application principale PyCompiler ARK.
+
+Fournit une interface utilisateur moderne permettant de:
+- Découvrir et lister les plugins BCASL disponibles
+- Activer/désactiver les plugins
+- Réordonner l'exécution des plugins
+- Exécuter les plugins de pré-compilation
+- Afficher les rapports d'exécution
+"""
+
+from __future__ import annotations
+
+from .gui import BcaslStandaloneGui
+from .app import BcaslOnlyModApp
+
+__version__ = "1.0.0"
+__author__ = "Ague Samuel Amen"
+
+__all__ = [
+    "BcaslStandaloneGui",
+    "BcaslOnlyModApp",
+]
+
