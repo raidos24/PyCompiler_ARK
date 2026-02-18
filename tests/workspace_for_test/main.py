40ebae7ab69f1a2b7271ecc834196cc6215101b9
Ague Samuel Amen <ague.samuel27@gmail.com>
2026-02-18 23:34:51 +0000

    main.py pour le workspace de test

---
+++ tests/workspace_for_test/main.py
@@ -0,0 +1,12 @@
+# ceci est un fichier de test présent dans le workspace de tests
+x = 1
+y = 34
+s = True
+
+if x < 3 and s == False :
+    print("test is not ok !")
+    
+elif x < 3 and s == True :
+    print("test is ok !")
+     
+    
